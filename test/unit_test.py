2e03e33ce603df5f7da6e7ed65f127cb83493092
Bagus Rexy W <bwibowo122@gmail.com>
2024-07-09 15:42:37 +0700

    Add whole new Unit Test
    Unit testing for all APIs can test this

--- test/unit_test.py
+++ test/unit_test.py
@@ -110,7 +110,9 @@ def test_address(app_client):
         "image": "https://img.freepik.com/free-photo/man-avatar-profile_24640-14044.jpg",
         "email": "john.doe@example.com"
     }
-    response = app_client.post('/customer', json=customer_1)  
+    response = app_client.post('/customer', json=customer_1)
+    
+    # Add new address for Customer 1  
     new_address = {
         "address" : "Kawasan Karyadeka Pancamurni Blok A Kav. 3",
         "district" : "Cikarang Selatan",
@@ -125,6 +127,7 @@ def test_address(app_client):
     assert response_data['response_code'] == 'SUCCESS'
     assert response_data['response_message'] == 'Success'
     
+    # Update Customer Data
     updated_customer = {
         "address" : "Kawasan Karyadeka Pancamurni Blok A Kav. 3",
         "district" : "Cikarang Selatan",
@@ -138,7 +141,8 @@ def test_address(app_client):
     response_data = response.get_json()
     assert response_data['response_code'] == 'SUCCESS'
     assert response_data['response_message'] == 'Success'
-
+    
+    # Delete Address Data
     response = app_client.delete(f'/address/{get_address_id.id}')
     assert response.status_code == 200
     response_data = response.get_json()
@@ -167,3 +171,98 @@ def generte_random_number():
     start_id = str(now_time) + str(hour_tm) + str(minute_tm) + str(second_tm)
     invoice_code = '{}'.format(str(start_id))
     return invoice_code
+
+
+def test_customers_detail(app_client):
+    # Create a Customer 1
+    customer_1 = {
+        "title": "Mr",
+        "name": "John Wick",
+        "gender": "M",
+        "phone_number": "08532222222255",
+        "image": "https://img.freepik.com/free-photo/man-avatar-profile_24640-14044.jpg",
+        "email": "john.doe@example.com"
+    }
+    response = app_client.post('/customer', json=customer_1)
+    assert response.status_code == 200
+    response_data = response.get_json()
+    assert response_data['response_code'] == 'SUCCESS'
+    assert response_data['response_message'] == 'Success'
+    
+    # Add new address for Customer 1  
+    new_address = {
+        "address" : "Kawasan Karyadeka Pancamurni Blok A Kav. 3",
+        "district" : "Cikarang Selatan",
+        "city" : "Jakarta Selatan",
+        "province" : "DKI Jakarta",
+        "postal_code" : 17530
+        }
+
+    response = app_client.post(f'/address?customer_id={1}', json=new_address)
+    assert response.status_code == 200
+    response_data = response.get_json()
+    assert response_data['response_code'] == 'SUCCESS'
+    assert response_data['response_message'] == 'Success'
+    
+    # Get Customer Detail
+    response = app_client.get('/customer/1')
+    assert response.status_code == 200
+    response_data = response.get_json()
+
+    assert response_data['response_code'] == 'SUCCESS'
+    assert response_data['response_message'] == 'Success'
+
+    customer_1_obj = Customer.query.filter(Customer.name == customer_1.get("name")).first()
+    get_address_id = db_address_query.get_one_address_by_customer_id(1)
+
+    expected_data = {
+        "data": {
+            "address": [
+                {
+                    "address": "Kawasan Karyadeka Pancamurni Blok A Kav. 3",
+                    "city": "Jakarta Selatan",
+                    "district": "Cikarang Selatan",
+                    "id": str(customer_1_obj.id),
+                    "postal_code": 17530,
+                    "province": "DKI Jakarta",
+                }
+            ],
+            "title": "Mr",
+            "name": "John Wick",
+            "gender": "M",
+            "phone_number": "08532222222255",
+            "image": "https://img.freepik.com/free-photo/man-avatar-profile_24640-14044.jpg",
+            "email": "john.doe@example.com"
+    },
+        "response_code": "SUCCESS",
+        "response_message": "Success",
+    }
+
+    assert response_data['data']['name'] == expected_data['data']['name']
+    assert response_data['data']['phone_number'] == expected_data['data']['phone_number']
+    assert response_data['data']['email'] == expected_data['data']['email']
+    assert response_data['data']['title'] == expected_data['data']['title']
+    assert response_data['data']['gender'] == expected_data['data']['gender']
+    assert response_data['data']['image'] == expected_data['data']['image']
+
+    assert len(response_data['data']['address']) == 1
+    assert response_data['data']['address'][0]['address'] == expected_data['data']['address'][0]['address']
+    assert response_data['data']['address'][0]['city'] == expected_data['data']['address'][0]['city']
+    assert response_data['data']['address'][0]['district'] == expected_data['data']['address'][0]['district']
+    assert response_data['data']['address'][0]['postal_code'] == expected_data['data']['address'][0]['postal_code']
+    assert response_data['data']['address'][0]['province'] == expected_data['data']['address'][0]['province']
+    
+    # Delete Address Data
+    response = app_client.delete(f'/address/{get_address_id.id}')
+    assert response.status_code == 200
+    response_data = response.get_json()
+    assert response_data['response_code'] == 'SUCCESS'
+    assert response_data['response_message'] == 'Success'
+    
+    # Delete Customer 1
+    customer_1_obj = Customer.query.filter(Customer.name == customer_1.get("name")).first()
+    response = app_client.delete(f'/customer/{str(customer_1_obj.customer_id)}')
+    assert response.status_code == 200
+    response_data = response.get_json()
+    assert response_data['response_code'] == 'SUCCESS'
+    assert response_data['response_message'] == 'Success'
